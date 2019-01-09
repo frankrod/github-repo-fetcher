b7c84558651ef783fd488fe4bd35449915aa1cc5
Frank <frankhn0801@gmail.com>
2019-01-08 21:45:41 -0600

    created top contributors card component

---
+++ src/components/TopContributorsCard/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { ContributorsType } from '../../containers/Contributors';
+import './TopContributorsCard.css';
+
+export interface Props {
+  contributor: ContributorsType;
+}
+
+const TopContributorsCard: React.FunctionComponent<Props> = ({
+  contributor,
+}) => (
+  <div className="top-contributors-card">
+    <img
+      className="card-avatar"
+      src={contributor.avatar_url}
+      alt="No avatar :("
+    />
+    <a className="top-contributors-username" href={contributor.html_url}>
+      {contributor.login}
+    </a>
+    <span className="top-contributors-contributions">
+      ({contributor.contributions.toLocaleString()} - contributions)
+    </span>
+  </div>
+);
+
+export default TopContributorsCard;
